4e7501f2ae136872f9403c9fe34c4e4ca0e32cb4
Jo Pearce <jo@jopearce.co.uk>
2016-12-11 12:42:39 +0000

    Eliminated some warnings and added a test for the case where an error response is empty.

--- InterFAX.Api.Test.Unit/HttpClientExtensionsTests.cs
+++ InterFAX.Api.Test.Unit/HttpClientExtensionsTests.cs
@@ -12,7 +12,7 @@ namespace InterFAX.Api.Test.Unit
         private MockHttpMessageHandler _handler;
 
         [Test]
-        public void should_throw_exception_on_error()
+        public void should_create_HttpError_from_error_response()
         {
             _handler = new MockHttpMessageHandler
             {
@@ -31,8 +31,32 @@ namespace InterFAX.Api.Test.Unit
             var apiException = exception.InnerExceptions[0] as ApiException;
             Assert.NotNull(apiException);
             Assert.AreEqual((int) _handler.ExpectedStatusCode, apiException.Error.Code);
-            Assert.AreEqual(_handler.ExpectedContent, apiException.Error.Message);
-            Assert.AreEqual(_handler.ExpectedReasonPhrase, apiException.Error.MoreInfo);
+            Assert.AreEqual(_handler.ExpectedReasonPhrase, apiException.Error.Message);
+            Assert.AreEqual(_handler.ExpectedContent, apiException.Error.MoreInfo);
+        }
+
+        [Test]
+        public void should_create_HttpError_from_empty_error_response()
+        {
+            _handler = new MockHttpMessageHandler
+            {
+                ExpectedContent = "",
+                ExpectedReasonPhrase = "Something was not found.",
+                ExpectedContentType = "text/plain",
+                ExpectedStatusCode = HttpStatusCode.NotFound,
+                ExpectedUri = new Uri("https://rest.interfax.net/accounts/self/ppcards/balance")
+            };
+
+            _interfax = new FaxClient("unit-test-user", "unit-test-pass", _handler);
+
+            var exception = Assert.Throws<AggregateException>(() => { var balance = _interfax.Account.GetBalance().Result; });
+            Assert.AreEqual(1, exception.InnerExceptions.Count);
+
+            var apiException = exception.InnerExceptions[0] as ApiException;
+            Assert.NotNull(apiException);
+            Assert.AreEqual((int) _handler.ExpectedStatusCode, apiException.Error.Code);
+            Assert.AreEqual(_handler.ExpectedReasonPhrase, apiException.Error.Message);
+            Assert.AreEqual("No content returned", apiException.Error.MoreInfo);
         }
     }
 }
