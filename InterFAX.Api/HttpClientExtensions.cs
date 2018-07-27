3ab6b1cf12c20102a6f479915b3f850dbfb26bdb
Kaelan Fouwels <kf@kaelanfouwels.com>
2018-07-27 16:22:38 +0100

    Move InterFax.Api to .NET standard 2.0

--- InterFAX.Api/HttpClientExtensions.cs
+++ InterFAX.Api/HttpClientExtensions.cs
@@ -4,7 +4,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web;
-using InterFAX.Api.Dtos;
+using InterFAX.Api.Dtos;  
 
 namespace InterFAX.Api
 {
